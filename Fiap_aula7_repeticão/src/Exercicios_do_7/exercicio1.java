215b68138fb907372c5815f28fbb09085f11bad1
Yan-Wendel <yanwendelrodriguesmachado@gmail.com>
2024-09-03 17:23:15 -0300

    ex

--- Fiap_aula7_repeticão/src/Exercicios_do_7/exercicio1.java
+++ Fiap_aula7_repeticão/src/Exercicios_do_7/exercicio1.java
@@ -1,9 +1,13 @@
 package Exercicios_do_7;
 
 public class exercicio1 {
-
+/*▪ Faça um programa que exiba a mensagem “Olá, Mundo”.
+▪ Essa mensagem deverá ser exibida repetidamente.
+▪ Ao final de toda iteração da repetição, você deve perguntar ao usuário se ele deseja
+exibir a mensagem novamente.
+▪ Se sim, exiba novamente. Senão, saia do loop e exiba a mensagem “Fim”.*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
