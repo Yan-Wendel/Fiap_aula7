1cae43c612ee3affcd83be8afa0adeb918e61d8b
Yan-Wendel <yanwendelrodriguesmachado@gmail.com>
2024-09-03 17:34:05 -0300

    ex

--- Fiap_aula7_repeticão/src/Exercicios_do_7/exercicio1.java
+++ Fiap_aula7_repeticão/src/Exercicios_do_7/exercicio1.java
@@ -1,5 +1,5 @@
 package Exercicios_do_7;
-
+import java.util.Scanner;
 public class exercicio1 {
 /*▪ Faça um programa que exiba a mensagem “Olá, Mundo”.
 ▪ Essa mensagem deverá ser exibida repetidamente.
@@ -7,8 +7,11 @@ public class exercicio1 {
 exibir a mensagem novamente.
 ▪ Se sim, exiba novamente. Senão, saia do loop e exiba a mensagem “Fim”.*/
 	public static void main(String[] args) {
-		
-
+		 Scanner ler = new Scanner(System.in);
+		 int i=0;
+		 while(i<10) {
+		 System.out.println("Digite o valor do lado A:");
+		 i++;
+		 }
 	}
-
 }
