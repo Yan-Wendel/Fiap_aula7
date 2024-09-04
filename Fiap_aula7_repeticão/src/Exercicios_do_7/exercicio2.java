a3c8b900c7ba64d27fb98ac446a9cfb3bbd567d8
Yan-Wendel <yanwendelrodriguesmachado@gmail.com>
2024-09-04 13:48:05 -0300

    ex

---
+++ Fiap_aula7_repeticão/src/Exercicios_do_7/exercicio2.java
@@ -0,0 +1,14 @@
+package Exercicios_do_7;
+import java.util.Scanner;
+public class exercicio2 {
+//▪ Contagem de 0 a 100 pulando de 10 em 10.
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int cc = 0;
+		while (cc < 101) {
+		System.out.println(cc);
+		cc=cc+10;
+		}
+	}
+}
